ca6847e21f0ac32483bdaa0f2cc5f53e670647c4
Honey Reddy <honeyreddymudireddy@gmail.com>
2025-03-12 17:32:46 +0000

    Variabels and Datatypes

---
+++ 01_BASICS/01_Variables.js
@@ -0,0 +1,16 @@
+const accountid = "12222488"
+let accountemail = "honeyreddymudireddy@gmail.com"
+var accountpassword = "1630"
+accountCity = "Jaipur"
+let accountstate;//prints undefined
+
+// accountid = "12221859"//Not allowed assignment to const variable
+accountemail = "mittalrehan0@gmail.com"
+accountpassword = "1421"
+accountCity = "Bengaluru"
+console.log(accountid)
+/*
+prefer not to use var 
+because of issue in block scope and fuctional scope
+*/
+console.log([accountid,accountemail,accountpassword,accountCity,accountstate])
---
+++ 01_BASICS/02_Datatypes.js
@@ -0,0 +1,7 @@
+"use strict" ; //treat all jscode as new version
+//alert("hello")  //used in google search engine
+console.log(3+3) //code readability should be high
+console.log("Honey")
+let name ="Honey"
+let age = 18
+
