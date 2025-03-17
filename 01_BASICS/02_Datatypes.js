67c30da844563632bf4abfaa6083f3928e02b6c7
Honey Reddy <honeyreddymudireddy@gmail.com>
2025-03-17 15:59:23 +0000

    datatypes

--- 01_BASICS/02_Datatypes.js
+++ 01_BASICS/02_Datatypes.js
@@ -4,4 +4,17 @@ console.log(3+3) //code readability should be high
 console.log("Honey")
 let name ="Honey"
 let age = 18
+let state = null
+//primitive data types
+//number 2 to power 53
+//bigint
+//string single quote or double quote
+//null ->stand alone value
+//undefined-->
+//symbom --> unique
 
+//object
+console.log(typeof "honey")
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
